de577f6aceb8e9fd06071ad535ab3b7c5a2631ba
muhammadchoifusanjaya <32087280+muhammadchoifusanjaya@users.noreply.github.com>
2017-09-25 13:34:33 +0700

    Add files via upload

---
+++ Tugas1-04317030(choifu).py
@@ -0,0 +1,9 @@
+nama = "muhammad choifu sanjaya"
+nim = "04317030"
+umur = 19
+print ("namaku:" + nama)
+print ("nimku:" + nim)
+print ("umurku:")
+print (umur)
+print ("umur 5 tahun lagi:")
+print (umur +5)
